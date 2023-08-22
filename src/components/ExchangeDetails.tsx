cef4de39ad6f05af321bda2009e860686da7a43b
Milan Vasic <milan.vasic.su@gmail.com>
2023-08-22 18:58:01 +0200

    feat(details): added responsiveness and missing details like social medai, year of establishment

--- src/components/ExchangeDetails.tsx
+++ src/components/ExchangeDetails.tsx
@@ -1,7 +1,7 @@
-// src/components/ExchangeDetails.tsx
 import React, { useState, useEffect } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import { getExchangeById } from "../gateway/api";
+import styled from "styled-components";
 
 type ExchangeDetailsParams = {
   id: string;
@@ -13,14 +13,50 @@ interface Exchange {
   trust_score_rank: number;
   image: string;
   year_established: number;
-  urls: {
-    website: string[];
-    twitter: string[];
-    facebook: string[];
-    reddit: string[];
-    description: string[];
-  };
+  description: string;
+  facebook_url: string;
+  reddit_url: string;
+  telegram_url: string;
+  slack_url: string;
 }
+
+const ExchangeDetailsWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  padding: 20px;
+`;
+
+const ExchangeDetailsContainer = styled.div`
+  padding: 20px;
+  display: flex;
+  width: 100%;
+  flex-direction: column;
+  align-items: center;
+  text-align: center;
+  border: 1px solid #ccc;
+  border-radius: 20px;
+
+  & > * {
+    margin-bottom: 10px;
+  }
+
+  img {
+    max-width: 100%;
+    height: auto;
+    margin-top: 20px;
+  }
+
+  div {
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    gap: 10px;
+    margin-top: 20px;
+
+    a {
+      color: #000;
+    }
+  }
+`;
 
 const ExchangeDetails: React.FC = () => {
   const navigate = useNavigate();
@@ -28,12 +64,12 @@ const ExchangeDetails: React.FC = () => {
   const [exchange, setExchange] = useState<Exchange | null>(null);
 
   useEffect(() => {
-    async function fetchExchangeDetails() {
+    const fetchExchangeDetails = async () => {
       if (id) {
         const data = await getExchangeById(id);
         setExchange(data);
       }
-    }
+    };
 
     fetchExchangeDetails();
   }, [id]);
@@ -43,10 +79,61 @@ const ExchangeDetails: React.FC = () => {
   }
 
   return (
-    <div>
-      <button onClick={() => navigate(-1)}>Back</button>
-      <h2>{exchange.name}</h2>
-    </div>
+    <ExchangeDetailsWrapper>
+      <ExchangeDetailsContainer>
+        <button onClick={() => navigate(-1)}>Back</button>
+        <h2>{exchange.name}</h2>
+        <p>Country: {exchange.country}</p>
+        <p>Trust Rank: {exchange.trust_score_rank}</p>
+        <img src={exchange.image} alt={exchange.name} />
+        <p>Year of Establishment: {exchange.year_established}</p>
+        <p>Description: {exchange.description}</p>
+        <div>
+          <p>
+            <a
+              href={
+                exchange.facebook_url !== "" ? exchange.facebook_url : undefined
+              }
+              rel="noreferrer"
+              target="_blank"
+            >
+              Facebook
+            </a>
+          </p>
+          <p>
+            <a
+              href={
+                exchange.reddit_url !== "" ? exchange.reddit_url : undefined
+              }
+              rel="noreferrer"
+              target="_blank"
+            >
+              Reddit
+            </a>
+          </p>
+          <p>
+            <a
+              href={
+                exchange.telegram_url !== "" ? exchange.telegram_url : undefined
+              }
+              rel="noreferrer"
+              target="_blank"
+            >
+              Telegram
+            </a>
+          </p>
+          <p>
+            <a
+              href={exchange.slack_url !== "" ? exchange.slack_url : undefined}
+              rel="noreferrer"
+              target="_blank"
+            >
+              Slack
+            </a>
+          </p>
+        </div>
+      </ExchangeDetailsContainer>
+    </ExchangeDetailsWrapper>
   );
 };
 
